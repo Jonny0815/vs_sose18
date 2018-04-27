c08a8a64fd7b9fad8a954030a4ed8d298a47fa56
Johannes Hitzinger <jonny0815ger@gmail.com>
2018-04-27 15:02:04 +0200

    server working ?

--- CentralServer/CentralServer/Server.cpp
+++ CentralServer/CentralServer/Server.cpp
@@ -3,9 +3,9 @@
 
 
 
-Server::Server()
+Server::Server(char* port_h)
 {
-
+	port = atoi(port_h);
 }
 
 
@@ -15,6 +15,25 @@ Server::~Server()
 
 void Server::run()
 {
+	boost::asio::io_service io_service;
+
+	udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
+	for (;;)
+	{
+		char data[1024];
+		udp::endpoint sender_endpoint;
+		size_t length = sock.receive_from(
+			boost::asio::buffer(data, 1024), sender_endpoint);
+
+		recieved_messurements++;
+
+		string data_s = data;
+
+
+	}
+
+
+
 
 
 }
--- CentralServer/CentralServer/Server.h
+++ CentralServer/CentralServer/Server.h
@@ -1,15 +1,30 @@
 #pragma once
 
+#include <cstdlib>
+#include <iostream>
+#inclulde <vector>
+#include <boost/asio.hpp>
 
+using boost::asio::ip::udp;
 
+using namepace std;
 
 class Server
 {
 public:
-	Server();
+	Server(char*);
 	~Server();
 
 	void run();
+
+
+private:
+	int port;
+
+	int carid;
+	int id;
+	vector<int> messurements;
+	int recieved_messurements;
 
 };
 
