574bc9e05945e6160aec7e86a6fd943efd709c05
Johannes Hitzinger <jonny0815ger@gmail.com>
2018-05-11 15:31:45 +0200

    reset

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -5,7 +5,6 @@
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
-#include <boost/bind.hpp>
 #include <vector>
 
 #include "ServerInst.h"
@@ -47,3 +46,151 @@ int main(int argc, char* argv[])
 
 
 }
+
+
+//
+//
+//#include <cstdlib>
+//#include <iostream>
+//#include <boost/bind.hpp>
+//#include <boost/asio.hpp>
+//
+//using boost::asio::ip::tcp;
+//
+//class session
+//{
+//public:
+//	session(boost::asio::io_service& io_service)
+//		: socket_(io_service)
+//	{
+//	}
+//
+//	tcp::socket& socket()
+//	{
+//		return socket_;
+//	}
+//
+//	void start()
+//	{
+//		socket_.async_read_some(boost::asio::buffer(data_, max_length),
+//			boost::bind(&session::handle_read, this,
+//				boost::asio::placeholders::error,
+//				boost::asio::placeholders::bytes_transferred));
+//	}
+//
+//private:
+//	void handle_read(const boost::system::error_code& error,
+//		size_t bytes_transferred)
+//	{
+//		if (!error)
+//		{
+//			boost::asio::async_write(socket_,
+//				boost::asio::buffer(data_, bytes_transferred),
+//				boost::bind(&session::handle_write, this,
+//					boost::asio::placeholders::error));
+//
+//
+//			std::cout << "1 "<< std::endl << data_ << std::endl;
+//
+//		}
+//		else
+//		{
+//			delete this;
+//		}
+//	}
+//
+//	void handle_write(const boost::system::error_code& error)
+//	{
+//		if (!error)
+//		{
+//
+//			std::string data_s = "HTTP/1.1 200 Ok \r\n Content-type: text/html \r\n Content-length: 39 \r\n\r\n hello world, account created and alive \r\n";
+//
+//			char* varchar = new char[data_s.size()];
+//
+//
+//			strncpy(varchar, data_s.c_str(), data_s.size());
+//
+//			std::cout << varchar << std::endl;
+//
+//			socket_.async_read_some(boost::asio::buffer(varchar, data_s.size()),
+//				boost::bind(&session::handle_read, this,
+//					boost::asio::placeholders::error,
+//					boost::asio::placeholders::bytes_transferred));
+//
+//			std::cout << "sended" << varchar << std::endl;
+//
+//		}
+//		else
+//		{
+//			delete this;
+//		}
+//	}
+//
+//	tcp::socket socket_;
+//	enum { max_length = 1024 };
+//	char data_[max_length];
+//};
+//
+//class server
+//{
+//public:
+//	server(boost::asio::io_service& io_service, short port)
+//		: io_service_(io_service),
+//		acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+//	{
+//		start_accept();
+//	}
+//
+//private:
+//	void start_accept()
+//	{
+//		session* new_session = new session(io_service_);
+//		acceptor_.async_accept(new_session->socket(),
+//			boost::bind(&server::handle_accept, this, new_session,
+//				boost::asio::placeholders::error));
+//	}
+//
+//	void handle_accept(session* new_session,
+//		const boost::system::error_code& error)
+//	{
+//		if (!error)
+//		{
+//			new_session->start();
+//		}
+//		else
+//		{
+//			delete new_session;
+//		}
+//
+//		start_accept();
+//	}
+//
+//	boost::asio::io_service& io_service_;
+//	tcp::acceptor acceptor_;
+//};
+//
+//int main(int argc, char* argv[])
+//{
+//	try
+//	{
+//		if (argc != 2)
+//		{
+//			std::cerr << "Usage: async_tcp_echo_server <port>\n";
+//			return 1;
+//		}
+//
+//		boost::asio::io_service io_service;
+//
+//		using namespace std; // For atoi.
+//		server s(io_service, atoi(argv[1]));
+//
+//		io_service.run();
+//	}
+//	catch (std::exception& e)
+//	{
+//		std::cerr << "Exception: " << e.what() << "\n";
+//	}
+//
+//	return 0;
+//}
