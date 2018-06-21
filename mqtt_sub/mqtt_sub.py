28e9dc37045221b3eac780d6478e0cafce2588c0
Jonny0815 <Jonny0815@JONNY0815>
2018-06-21 17:28:22 +0200

    running with sync

--- mqtt_sub/mqtt_sub.py
+++ mqtt_sub/mqtt_sub.py
@@ -1,5 +1,5 @@
-from typing import List, Any
-
+from typing import List, Any, Set
+from time import sleep
 import paho.mqtt.client as mqtt
 from xmlrpc.server import SimpleXMLRPCServer
 import argparse
@@ -24,22 +24,39 @@ class Measurement:
     def to_string(self):
         return str("Card-ID: " + self.car + " Sensor-ID: " + self.sensor + " Value: " + self.value + " Timestamp: " + self.timestamp)
 
-    def to_dict(self):
-        return dict(car=self.car, sensor=self.sensor,value = self.value, timestamp=self.timestamp)
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--target', help="target mqtt host", required=True)
 args = parser.parse_args()
 
-measurements: List[Measurement] = list()
+measurements = set()
+my_request = False
+
+
+def send_updates(car, sensor):
+    sensor = sensor[:-1]
+    for each in measurements:
+        print("car: " + car + " each.car: " + each.car + " sensor: " + sensor + " each.sensor " + each.sensor)
+        if car == each.car and sensor == each.sensor:
+            print("pub update")
+            pub.publish("update", each.car + " " + each.sensor + " " + each.value + " " + each.timestamp)
 
 
 def get_data(car, sensor):
+
+    global my_request
+    my_request = True
+    print("about to pub.single")
+
+    pub.publish("update", "GET " + str(car) + " " + str(sensor))
+
+    sleep(2)
+
     to_return = list()
-    for x in range(len(measurements)):
-        if car == measurements[x].car and sensor == measurements[x].sensor:
-            to_return.append(measurements[x].to_string())
+    for each in measurements:
+        if car == each.car and sensor == each.sensor:
+            to_return.append(each.to_string())
+    my_request = False
     return to_return
 
 
@@ -47,24 +64,39 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     client.subscribe("test")
+    client.subscribe("update")
 
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
+    global my_request
+
     data = str(msg.payload)
     data_l = list(data.split(" "))
 
     data_l[0] = data_l[0][-3:]
-    data_l[3] = data_l[3][:-2]
 
-    measurements.append(Measurement(data_l[0], data_l[1], data_l[2], data_l[3]))
+    if msg.topic == "update":
+        if data_l[0] == "GET":
+            if my_request is False:
+                print("about so send updates")
+                send_updates(data_l[1], data_l[2])
+        else:
+            data_l[3] = data_l[3][:-2]
+            measurements.add(Measurement(data_l[0], data_l[1], data_l[2], data_l[3]))
+
+    if msg.topic == "test":
+        data_l[3] = data_l[3][:-2]
+        measurements.add(Measurement(data_l[0], data_l[1], data_l[2], data_l[3]))
 
 
+pub = mqtt.Client()
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+pub.connect(args.target, 1883, 60)
 client.connect(args.target, 1883, 60)
 
 server = SimpleXMLRPCServer(("localhost", 8000))
